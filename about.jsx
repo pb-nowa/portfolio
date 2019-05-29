31f511240198b5315d3a1dbd90bc03c4842436ab
PNowakowski <p.nowakowskimusic@gmail.com>
2019-05-28 20:48:32 -0700

    Update colors

---
+++ about.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const about = () => {
+    return(
+        <div>
+            <h1>ABOUT ME</h1>
+            <p>here are some facts about me. this will be a page about how i am such a good coder and stuff. here are more words so that I cn fill up this page without habing to type any actual substance on the page. wow such a nice paragraph about me. yay!!!1 :D</p>
+        </div>
+    )
+}
+
+export default about;
--- root.jsx
+++ root.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Project from './projects';
+import About from './about';
 import { HashRouter, Switch, Route, Link } from 'react-router-dom';
 
 class Root extends React.Component { 
@@ -14,13 +15,16 @@ class Root extends React.Component {
                         <Link to="/about">
                             <h2>ABOUT</h2>
                         </Link>
-                        <Link to="/projects">
+                        <Link to="/projects/granular-synth">
                             <h2>PROJECTS</h2>
                         </Link>
                     </ul>
                 </div>
                 <Switch>
-                    <Route exact path="/projects" component={Project}/>
+                    <Route exact path="/projects/granular-synth" component={Project}/>
+                    <Route exact path="/projects/pokenalysis" component={Project}/>
+                    <Route exact path="/projects/alldays" component={Project}/>
+                    <Route exact path="/about" component={About}/>
                 </Switch>
             </div>
         </HashRouter>
