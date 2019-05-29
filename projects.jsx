b8771baddd9978369a6c93326ad28c5ce8c84797
PNowakowski <p.nowakowskimusic@gmail.com>
2019-05-28 20:16:11 -0700

    Add links

--- projects.jsx
+++ projects.jsx
@@ -2,15 +2,29 @@ import React from 'react';
 
     const project = () => {
         return(
+            <>
+            <ul className="tabs-container">
+                <li id="first" className="project-tab">
+                    <h2>Project1</h2>
+                </li>
+                <li id="second" className="project-tab">
+                    <h2>Project2</h2>
+                </li>
+                <li id="third" className="project-tab">
+                    <h2>Project3</h2>
+                </li>
+            </ul>
             <div className="display-container">
                 <div className="project-about">
                     <h1>Project Title</h1>
                     <p>This is a paragraph that is describing my project and how awesome it is. It will be some length and have a few bullet points. Here are a few more words in order to fill up some of this space. I need more words to add to this paragraph. Wow. what a grat project !!!!!! :D</p>
+                    <a className="explore" href="">Explore</a>
                 </div>
                 <div className="project-image-container">
                     <img className="project-image" src="https://raw.githubusercontent.com/pb-nowa/Virtual-Synthesizer/master/assets/screenshots/granular-synth-destructure.gif" alt="project 1"/>
                 </div>
             </div>
+            </>
         );
     }
     
--- root.jsx
+++ root.jsx
@@ -1,19 +1,29 @@
 import React from 'react';
 import Project from './projects';
+import { HashRouter, Switch, Route, Link } from 'react-router-dom';
 
-class Root extends React.Component {
+class Root extends React.Component { 
 
     render(){
         return(
+        <HashRouter>
             <div className="main">
-                <div className="header">PRESTON NOWAKOWSKI</div>
-                <ul className="tabs-container">
-                    <li className="project-tab">Project1</li>
-                    <li className="project-tab">Project2</li>
-                    <li className="project-tab">Project3</li>
-                </ul>
-                <Project/>
+                <div className="header">
+                    <h1>PRESTON NOWAKOWSKI</h1>
+                    <ul className="toolbar">
+                        <Link to="/about">
+                            <h2>ABOUT</h2>
+                        </Link>
+                        <Link to="/projects">
+                            <h2>PROJECTS</h2>
+                        </Link>
+                    </ul>
+                </div>
+                <Switch>
+                    <Route exact path="/projects" component={Project}/>
+                </Switch>
             </div>
+        </HashRouter>
         )
     }
 
