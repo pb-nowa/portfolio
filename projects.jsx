364bda359fba6d089c4ce39bc69a5d0a0b75df90
PNowakowski <p.nowakowskimusic@gmail.com>
2019-05-28 19:13:51 -0700

    Update fonts

---
+++ projects.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+    const project = () => {
+        return(
+            <div className="display-container">
+                <div className="project-about">
+                    <h1>Project Title</h1>
+                    <p>This is a paragraph that is describing my project and how awesome it is. It will be some length and have a few bullet points. Here are a few more words in order to fill up some of this space. I need more words to add to this paragraph. Wow. what a grat project !!!!!! :D</p>
+                </div>
+                <div className="project-image-container">
+                    <img className="project-image" src="https://raw.githubusercontent.com/pb-nowa/Virtual-Synthesizer/master/assets/screenshots/granular-synth-destructure.gif" alt="project 1"/>
+                </div>
+            </div>
+        );
+    }
+    
+
+export default project;
--- root.jsx
+++ root.jsx
@@ -1,11 +1,18 @@
 import React from 'react';
+import Project from './projects';
 
 class Root extends React.Component {
 
     render(){
         return(
-            <div>
-                working
+            <div className="main">
+                <div className="header">PRESTON NOWAKOWSKI</div>
+                <ul className="tabs-container">
+                    <li className="project-tab">Project1</li>
+                    <li className="project-tab">Project2</li>
+                    <li className="project-tab">Project3</li>
+                </ul>
+                <Project/>
             </div>
         )
     }
