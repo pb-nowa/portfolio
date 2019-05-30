a182011f5fcbc3128a51764fcf3c568961cad2f8
PNowakowski <p.nowakowskimusic@gmail.com>
2019-05-29 18:52:09 -0700

    Update profile with skills and better headers

--- about.jsx
+++ about.jsx
@@ -10,8 +10,13 @@ const about = () => {
                         <a href="https://github.com/pb-nowa"><h3>Github</h3></a>
                         <a href="https://www.linkedin.com/in/preston-nowakowski/"><h3>LinkedIn</h3></a>
                 </div>
-                <img className="profile"src="https://media.licdn.com/dms/image/C5103AQGlAnULHP8wNg/profile-displayphoto-shrink_200_200/0?e=1562803200&v=beta&t=_s9kbUl9VxFfr_ullYAXLXMvMLOizL0K0TEz0V18IIQ" alt="me"/>
-                <p>here are some facts about me. this will be a page about how i am such a good coder and stuff. here are more words so that I cn fill up this page without habing to type any actual substance on the page. wow such a nice paragraph about me. yay!!!1 :D</p>
+                <div className="body">
+                    <img className="profile"src="https://media.licdn.com/dms/image/C5103AQGlAnULHP8wNg/profile-displayphoto-shrink_200_200/0?e=1562803200&v=beta&t=_s9kbUl9VxFfr_ullYAXLXMvMLOizL0K0TEz0V18IIQ" alt="me"/>
+                    <h2>SKILLS</h2>
+                    <p>Ruby,  Ruby on Rails,  JavaScript,  React,  Redux,  jQuery,  AJAX, HTML,  CSS,  SCSS,  SQL, MongoDB, Express, git</p>
+                    <h2>BACKGROUND</h2>
+                    <p>here are some facts about me. this will be a page about how i am such a good coder and stuff. here are more words so that I cn fill up this page without habing to type any actual substance on the page. wow such a nice paragraph about me. yay!!!1 :D</p>
+                </div>
             </div>
             <div className="about-right">
                 <div className="about-header-right"></div>
