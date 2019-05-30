7087cc4eb23708611f04db28e1a79609ae4f9a2e
PNowakowski <p.nowakowskimusic@gmail.com>
2019-05-29 19:14:45 -0700

    Update about right-side formatting

--- about.jsx
+++ about.jsx
@@ -12,14 +12,19 @@ const about = () => {
                 </div>
                 <div className="body">
                     <img className="profile"src="https://media.licdn.com/dms/image/C5103AQGlAnULHP8wNg/profile-displayphoto-shrink_200_200/0?e=1562803200&v=beta&t=_s9kbUl9VxFfr_ullYAXLXMvMLOizL0K0TEz0V18IIQ" alt="me"/>
-                    <h2>SKILLS</h2>
-                    <p>Ruby,  Ruby on Rails,  JavaScript,  React,  Redux,  jQuery,  AJAX, HTML,  CSS,  SCSS,  SQL, MongoDB, Express, git</p>
-                    <h2>BACKGROUND</h2>
-                    <p>here are some facts about me. this will be a page about how i am such a good coder and stuff. here are more words so that I cn fill up this page without habing to type any actual substance on the page. wow such a nice paragraph about me. yay!!!1 :D</p>
+                    <h2>TECHNOLOGIES</h2>
+                    <h3>LANGUAGES</h3>
+                    <p>Ruby, JavaScript</p>
+                    <h3>BACKEND</h3>
+                    <p>Ruby, Ruby on Rails, jQuery, AJAX, Express, SQL, MongoDB </p>
+                    <h3>FRONTEND</h3>
+                    <p>JavaScript,  React,  Redux, HTML,  CSS,  SCSS, git</p>
                 </div>
             </div>
             <div className="about-right">
                 <div className="about-header-right"></div>
+                <h2>BACKGROUND</h2>
+                <p>here are some facts about me. this will be a page about how i am such a good coder and stuff. here are more words so that I cn fill up this page without habing to type any actual substance on the page. wow such a nice paragraph about me. yay!!!1 :D</p>
             </div>
         </div>
         <div className="footer">
