4848252765260346fde3463ba5ddaa3d959df278
PNowakowski <p.nowakowskimusic@gmail.com>
2019-06-04 12:19:06 -1000

    Update details for alldays

--- alldays.jsx
+++ alldays.jsx
@@ -56,11 +56,15 @@ class Alldays extends React.Component {
                             <p>Alldays is an online shoe store modelled after Allbirds.com. It is built on a Rails/PostgreSQL backend with a React/Redux frontend.</p>
                             <h3 className="features-title">FEATURES</h3>
                             <ul className="features-list">
-                                <li className="feature-item">Clean minimalist layout that keeps the focus on the product with a distraction free shopping experience</li>
-                                <li className="feature-item">Communicative animations that controls information and helps any user in their shopping experience</li>
-                                <li className="feature-item">Applied backend that will save customer information and keeps track of their purchases and shopping cart items</li>
+                                <li className="feature-item">&#8226; Clean minimalist layout that keeps the focus on the product with a distraction free shopping experience</li>
+                                <li className="feature-item">&#8226; Communicative animations that control information flow and help any user in their shopping experience</li>
+                                <li className="feature-item">&#8226; Applied backend that will save customer information and keep track of their purchases and shopping cart items</li>
                             </ul>
                             <a className="explore" href="http://alldays.herokuapp.com/#/"><h2>Explore</h2></a>
+                            <h3 className="features-title">DETAILS</h3>
+                            <p>Be sure to checkout the <a className="hyperlink" href="http://alldays.herokuapp.com/#/">website</a> or 
+                                visit the <a className="hyperlink" href="https://github.com/pb-nowa/All-Days">Github</a> for a more detailed look at Alldays.
+                            </p>
                         </div>
                         <ul className="technologies">
                             <img className="tech-icon rails" src="images/rails.png" alt="rails" />
